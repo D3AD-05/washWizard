6fd95eed895b24ecfa5d8347ee6d68901c2afbe5
Dias <daisvalooran@gmail.com>
2023-10-02 19:13:43 +0530

    trial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var mainSwiper = new Swiper(".mainSwiper", {
+  slidesPerView: 1,
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: " .swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // thumbs: {
+  //   swiper: thumbSwiper,
+  // },
+});
+
+var thumbSwiper = new Swiper(".thumbSwiper", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  effect: "fade",
+  fadeEffect: { crossFade: true },
+});
+
+mainSwiper.controller.control = thumbSwiper;
+thumbSwiper.controller.control = mainSwiper;
